f0117ec62855c0078bb5e8e7d8ee3199cd52b7e3
JonaJJSJ-crypto <78813250+JonaJJSJ-crypto@users.noreply.github.com>
2022-02-20 14:43:01 -0500

    Remove Verbose Comentary and include Validation checks in the code

--- Schedule_Payment.py
+++ Schedule_Payment.py
@@ -1,76 +1,75 @@
 #Implementation for a payment Based on schedule
-#This program accepts a .txt file with the next Format:
-#NAME=DDhh:mm-hh:mm,DDhh:mm-hh:mm
 #Author: Jonathan Sánchez
 #Github Repo: JonaJJSJ-crypto/SchedulePayment
 
 #Reading file with names and Schedules
-#Example of a line in the file: ASTRID=MO10:00-12:00,TH12:00-14:00,SU20:00-21:00
+#Example of correct entry in the file: ASTRID=MO10:00-12:00,TH12:00-14:00,SU20:00-21:00
 myFile = open("Schedule.txt","r")
 
 #Setting Days Type
 RegularDays = ['MO','TU','WE','TH','FR']
 WeekEndDays = ['SA','SU']
 #List of tuple consist of (final_hour_of_the_schedule,payment_of_the_schedule)
-#Final_hour_of_the_schedule is the total of minutes of that hours
 Minutes = [(540,25),(1080,15),(1440,20)]
 #Extra fee that is granted for working during weekends
 WeekEndExtraFee = 5
 
-#Recieves a string of the form (hh:mm) in 24 hours format and returns the total of minutes since (00:00)
+#Transform time (hh:mm) in 24h format to Minutes
 def MinuteFormat(Hour_Format):
-    if Hour_Format == "00:00":
-        return 1440
-    else:
-        Hour=int(Hour_Format[0:2])
-        Min=int(Hour_Format[3:5])
-        return Min + Hour*60
+    try:
+        Hour = int(Hour_Format[0:2])
+        Min = int(Hour_Format[3:5])
+        TimeMin = Min + Hour*60
+        if Hour_Format == "00:00":
+            return 1440
+        elif TimeMin < 0 or TimeMin > 1440:
+            return "Hour out of bounds"
+        else:
+            return Min + Hour*60
 
-#Validate that working Hours
-#def ValidHours(TimeBegin,TimeEnd):
-    #if TimeEnd-TimeBegin < 0:
+    except ValueError:
+        print("\n !!!Error: incorrect hour_format: {}".format(Hour_Format),"!!! Correct format: hh:mm")
 
 
-#Recieves a string of the form DDhh:mm-hh:mm & a boolean that confirms if is WeekEnd schedule
-#Returns The total payment from the working schedule
+#Given working hours (hh:mm-hh:mm) & IsWeekEnd flag, returns payment
 def payment(myTimes,IsWeekend):
     WEFee = WeekEndExtraFee
     if not IsWeekend:
         WEFee=0
     myTimeBegin=myTimes[0:5]
     myTimeEnd=myTimes[6:11]
-    # Change working hours to int Minute format
-    myTimeBegin= MinuteFormat(myTimeBegin)
-    myTimeEnd= MinuteFormat(myTimeEnd)
-    #Total worked time
-    TMinutes=myTimeEnd-myTimeBegin
-    # Print the Schedule that will be use to calculate the payment
-    ###print("Begin Time: ", myTimeBegin)
-    ###print("End Time: ",myTimeEnd)
-    ###print("Total Hours: ",TMinutes)
+    try:
+        myTimeBegin= MinuteFormat(myTimeBegin)
+        myTimeEnd= MinuteFormat(myTimeEnd)
+        TMinutes=myTimeEnd-myTimeBegin
+        if TMinutes < 0 or TMinutes > 1440:
+            return "Incorrect working hours"
+        #Internal Variable that holds the payment
+        pay=0;
 
-    #Internal Variable that holds the payment
-    pay=0;
+        for x in Minutes:
+            if myTimeBegin<=x[0]:
 
-    for x in Minutes:
-        if myTimeBegin<=x[0]:
-            #Work schedule starts and finishes in the same schedule
-            if myTimeBegin+TMinutes<=x[0]:
-                pay+= (x[1]+WEFee)*TMinutes/60
-                TMinutes=0;
-                myTimeBegin=1441;
-            #When work hours overlap in diferent schedules prepare for next schedule
-            else:
-                pay+= (x[1]+WEFee)*(x[0]-myTimeBegin)/60
-                myTimeBegin=x[0]+1
-                TMinutes=myTimeEnd-x[0]
-    print("     Pay for the Schedule: {}".format(pay))
+                #Work hours starts and finishes in the same schedule
+                if myTimeBegin+TMinutes<=x[0]:
+                    pay+= (x[1]+WEFee)*TMinutes/60
+                    TMinutes=0;
+                    myTimeBegin=1441;
 
-    return pay
+                #When work hours overlap in diferent schedules prepare for next schedule
+                else:
+                    pay+= (x[1]+WEFee)*(x[0]-myTimeBegin)/60
+                    myTimeBegin=x[0]+1
+                    TMinutes=myTimeEnd-x[0]
+        print("     Pay for the Schedule: {}".format(round(pay)))
 
-#search for each line "entry" in the File.
+        return pay
+
+    except TypeError:
+        print("\n !!!Error: incorrect working hours format: {}".format(myTimes),"!!! Correct format: hh:mm-hh:mm")
+
+#Calculate payment for each worked in the file
 for x in myFile:
-    #Saves the entry for manipulation
     mySchedule = x
     myName = mySchedule[0:mySchedule.find('=')]
     print(myName)
@@ -78,28 +77,28 @@ for x in myFile:
 
     #internal variable to store payment
     Total_payment=0
+    try:
+        for y in RegularDays:
+            #Search in the entry for multiple appearances of the same day
+            while mySchedule.find(y) != -1:
+                myTimes = mySchedule[mySchedule.find(y)+2:mySchedule.find(y)+13]
+                if mySchedule.find(y) != -1:
+                    print(y,' ',myTimes,end='')
+                    Total_payment += payment(myTimes,False)
+                    mySchedule = mySchedule[0:mySchedule.find(y)]+mySchedule[mySchedule.find(y)+14:len(mySchedule)]
+        for y in WeekEndDays:
+            #Search in the entry for multiple appearances of the same day
+            while mySchedule.find(y) != -1:
+                myTimes = mySchedule[mySchedule.find(y)+2:mySchedule.find(y)+13]
+                if mySchedule.find(y) != -1:
+                    print(y,' ',myTimes,end='')
+                    Total_payment += payment(myTimes,True)
+                    mySchedule = mySchedule[0:mySchedule.find(y)]+mySchedule[mySchedule.find(y)+14:len(mySchedule)]
+        if len(mySchedule) != 0:
+            print("\n !!!Error: Working day not found in: {}".format(mySchedule),"!!! Correct days: MO,TU,WE,TH,FR,SA,SU")
 
-    for y in RegularDays:
-        #Search in the entry for multiple appearances of the same day
-        while mySchedule.find(y) != -1:
-            #Stores the Day and time in the format DDhh:mm
-            myRTimes = mySchedule[mySchedule.find(y)+2:mySchedule.find(y)+13]
-            if mySchedule.find(y) != -1:
-                print(y,' ',myRTimes,end='')
-                Total_payment += payment(myRTimes,False)
-                #Re-stores the Schedule entry ommiting the analized day and workhours
-                mySchedule = mySchedule[0:mySchedule.find(y)]+mySchedule[mySchedule.find(y)+14:len(mySchedule)]
+    except TypeError:
+        print("\n !!!Error: incorrect schedule format: {}".format(mySchedule),"!!! Correct format: DDhh:mm-hh:mm")
 
-    for y in WeekEndDays:
-        #Search in the entry for multiple appearances of the same day
-        while mySchedule.find(y) != -1:
-            #Stores the Day and time in the format DDhh:mm
-            myWETimes = mySchedule[mySchedule.find(y)+2:mySchedule.find(y)+13]
-            if mySchedule.find(y) != -1:
-                print(y,' ',myWETimes,end='')
-                Total_payment += payment(myWETimes,True)
-                #Re-stores the Schedule entry ommiting the analized day and Schedule
-                #This allows multpiple entries of time in the same day
-                mySchedule = mySchedule[0:mySchedule.find(y)]+mySchedule[mySchedule.find(y)+14:len(mySchedule)]
 
-    print("The amount to pay {}".format(myName),"is: {}".format(Total_payment),"USD.\n\n")
+    print("The amount to pay {}".format(myName),"is: {}".format(round(Total_payment)),"USD.\n\n")
